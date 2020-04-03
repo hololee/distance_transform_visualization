68988664e7b5fe2af69ff4d14f17b8ae5be2d6e3
jonghyeok lee <30971523+hololee@users.noreply.github.com>
2020-04-03 21:46:07 +0900

    init

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from itertools import product
 from copy import deepcopy
 import matplotlib.pyplot as plt
+from skimage import draw
 
 """
 image  = (N, N), value is distance.
@@ -10,9 +11,6 @@ centers = tuple of centers.
 """
 
 # configure ----------------------------------------------------------------------
-
-# margion of center points.
-center_margin = 20
 
 # maximum_val
 N_max = np.inf
@@ -41,7 +39,7 @@ def chessboard_distance(*xys):
     :param xys: (x1, x2, y1, y2)
     :return:
     """
-    distance = np.max(np.abs(xys[0] - xys[1]), np.abs(xys[2] - xys[3]))
+    distance = np.max([np.abs(xys[0] - xys[1]), np.abs(xys[2] - xys[3])])
     return distance
 
 
@@ -70,14 +68,23 @@ distance_type = {"D_E": euclidean_distance,
 def generate_image(use_sample=True, init_img=None):
     # TODO: generate random image.
 
-    sample = np.array([[0, 0, 0, 0, 0, 0, 1, 0],
-                       [0, 0, 0, 0, 0, 1, 0, 0],
-                       [0, 0, 0, 0, 0, 1, 0, 0],
-                       [0, 0, 0, 0, 0, 1, 0, 0],
-                       [0, 1, 1, 0, 0, 0, 1, 0],
-                       [0, 1, 0, 0, 0, 0, 0, 1],
-                       [0, 1, 0, 0, 0, 0, 0, 0],
-                       [0, 1, 0, 0, 0, 0, 0, 0]
+    # sample = np.array([[0, 0, 0, 0, 0, 0, 1, 0],
+    #                    [0, 0, 0, 0, 0, 1, 0, 0],
+    #                    [0, 0, 0, 0, 0, 1, 0, 0],
+    #                    [0, 0, 0, 0, 0, 1, 0, 0],
+    #                    [0, 1, 1, 0, 0, 0, 1, 0],
+    #                    [0, 1, 0, 0, 0, 0, 0, 1],
+    #                    [0, 1, 0, 0, 0, 0, 0, 0],
+    #                    [0, 1, 0, 0, 0, 0, 0, 0]
+    #                    ], dtype=np.float)
+
+    sample = np.array([[0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0],
                        ], dtype=np.float)
     if use_sample:
         data = sample
@@ -87,7 +94,11 @@ def generate_image(use_sample=True, init_img=None):
         x_coord = np.random.randint(0, init_img["size"], init_img["centers"])
         y_coord = np.random.randint(0, init_img["size"], init_img["centers"])
 
-        data[y_coord, x_coord] = 1
+        for index in range(len(x_coord)):
+            # Create an outer and inner circle. Then subtract the inner from the outer.
+            radius = 1
+            ri, ci = draw.circle(x_coord[index], y_coord[index], radius=radius, shape=data.shape)
+            data[ri, ci] = 1
 
     copy_image = deepcopy(data)
 
@@ -133,7 +144,11 @@ def calculate_distance_al(sel_distance, sel_image):
 
             local_mask_center = copy_img[y, x]
 
-            copy_img[y, x] = np.min([local_mask_1, local_mask_2, local_mask_3, local_mask_4, local_mask_center])
+            try:
+                copy_img[y, x] = np.min(
+                    [local_mask_1, local_mask_2, local_mask_3, local_mask_4, local_mask_center]).item()
+            except:
+                copy_img[y, x] = np.min([local_mask_1, local_mask_2, local_mask_3, local_mask_4, local_mask_center])
 
     return copy_img
 
@@ -173,7 +188,11 @@ def calculate_distance_br(sel_distance, sel_image):
 
             local_mask_center = copy_img[y, x]
 
-            copy_img[y, x] = np.min([local_mask_1, local_mask_2, local_mask_3, local_mask_4, local_mask_center])
+            try:
+                copy_img[y, x] = np.min(
+                    [local_mask_1, local_mask_2, local_mask_3, local_mask_4, local_mask_center]).item()
+            except:
+                copy_img[y, x] = np.min([local_mask_1, local_mask_2, local_mask_3, local_mask_4, local_mask_center])
 
     return copy_img
 
@@ -183,15 +202,21 @@ def calculate_distance_br(sel_distance, sel_image):
 # # 1. Normal sample.---------------------------------------------
 # generate random centers.
 image = generate_image()
-result_AL = calculate_distance_al(distance_type["D_4"], image)
-final_result = calculate_distance_br(distance_type["D_4"], result_AL)
+result_AL = calculate_distance_al(distance_type["D_QE"], image)
+final_result = calculate_distance_br(distance_type["D_QE"], result_AL)
+
+result_AL = calculate_distance_al(distance_type["D_QE"], final_result)
+final_result = calculate_distance_br(distance_type["D_QE"], result_AL)
+
+result_AL = calculate_distance_al(distance_type["D_QE"], final_result)
+final_result = calculate_distance_br(distance_type["D_QE"], result_AL)
 
 plt.subplot(1, 2, 1)
 plt.imshow(image)
 ax = plt.gca()
 for y in range(image.shape[0]):
     for x in range(image.shape[1]):
-        ax.text(x, y, image[y, x], color='white', ha='center', va='center')
+        ax.text(x, y, str(image[y, x])[:3], color='white', ha='center', va='center')
 plt.title("Sample image")
 
 plt.subplot(1, 2, 2)
@@ -199,24 +224,105 @@ plt.imshow(final_result)
 ax = plt.gca()
 for y in range(final_result.shape[0]):
     for x in range(final_result.shape[1]):
-        ax.text(x, y, final_result[y, x], color='white', ha='center', va='center')
+        ax.text(x, y, str(final_result[y, x])[:3], color='white', ha='center', va='center')
 
-plt.title("Sample image with D_E")
+# plt.title("Sample image with D_E")
 plt.show()
 
-# # 1. DE sample. ---------------------------------------------
-# generate random centers.
-image = generate_image(use_sample=False, init_img={"centers": 3, "size": 128})
-result_AL = calculate_distance_al(distance_type["D_4"], image)
-final_result = calculate_distance_br(distance_type["D_4"], result_AL)
-
-X, Y = np.meshgrid(list(range(image.shape[0])), list(range(image.shape[1])))
-
-plt.subplot(1, 2, 1)
-plt.title("Random ones")
-plt.imshow(image)
-plt.subplot(1, 2, 2)
-plt.imshow(final_result)
-# plt.contour(X, Y, final_result[Y, X], 8, colors='white', linewidth=.2)
-plt.title("Random ones with D_4")
-plt.show()
+# # # 1. DE sample. ---------------------------------------------
+#
+# iterations = 60
+#
+# image_option = {"centers": 3, "size": 16}
+#
+# # generate random centers.
+# image = generate_image(use_sample=False, init_img=image_option)
+#
+#
+# result_AL = calculate_distance_al(distance_type["D_E"], image)
+# final_result = calculate_distance_br(distance_type["D_E"], result_AL)
+#
+# for i in range(iterations):
+#     result_AL = calculate_distance_al(distance_type["D_E"], final_result)
+#     final_result = calculate_distance_br(distance_type["D_E"], result_AL)
+#
+# X, Y = np.meshgrid(list(range(image.shape[0])), list(range(image.shape[1])))
+#
+# plt.subplot(1, 3, 1)
+# plt.title("Random ones")
+# plt.imshow(image)
+# plt.subplot(1, 3, 2)
+# plt.title("Random ones")
+# plt.imshow(result_AL)
+# plt.subplot(1, 3, 3)
+# plt.imshow(final_result)
+# plt.contour(X, Y, final_result[Y, X], 12, colors='white', linewidths=.2)
+# plt.title("Random ones with D_E")
+# plt.show()
+#
+# # generate random centers.
+# # image = generate_image(use_sample=False, init_img=image_option)
+# result_AL = calculate_distance_al(distance_type["D_4"], image)
+# final_result = calculate_distance_br(distance_type["D_4"], result_AL)
+# for i in range(iterations):
+#     result_AL = calculate_distance_al(distance_type["D_4"], final_result)
+#     final_result = calculate_distance_br(distance_type["D_4"], result_AL)
+#
+# X, Y = np.meshgrid(list(range(image.shape[0])), list(range(image.shape[1])))
+#
+# plt.subplot(1, 3, 1)
+# plt.title("Random ones")
+# plt.imshow(image)
+# plt.subplot(1, 3, 2)
+# plt.title("Random ones")
+# plt.imshow(result_AL)
+# plt.subplot(1, 3, 3)
+# plt.imshow(final_result)
+# plt.contour(X, Y, final_result[Y, X], 8, colors='white', linewidths=.2)
+# plt.title("Random ones with D_4")
+# plt.show()
+#
+# # generate random centers.
+# # image = generate_image(use_sample=False, init_img=image_option)
+# result_AL = calculate_distance_al(distance_type["D_8"], image)
+# final_result = calculate_distance_br(distance_type["D_8"], result_AL)
+# for i in range(iterations):
+#     result_AL = calculate_distance_al(distance_type["D_8"], final_result)
+#     final_result = calculate_distance_br(distance_type["D_8"], result_AL)
+#
+# X, Y = np.meshgrid(list(range(image.shape[0])), list(range(image.shape[1])))
+#
+# plt.subplot(1, 3, 1)
+# plt.title("Random ones")
+# plt.imshow(image)
+# plt.subplot(1, 3, 2)
+# plt.title("Random ones")
+# plt.imshow(result_AL)
+# plt.subplot(1, 3, 3)
+# plt.imshow(final_result)
+# plt.contour(X, Y, final_result[Y, X], 8, colors='white', linewidths=.2)
+# plt.title("Random ones with D_8 ")
+# plt.show()
+#
+# # generate random centers.
+# # image = generate_image(use_sample=False, init_img=image_option)
+# result_AL = calculate_distance_al(distance_type["D_QE"], image)
+# final_result = calculate_distance_br(distance_type["D_QE"], result_AL)
+#
+# for i in range(iterations):
+#     result_AL = calculate_distance_al(distance_type["D_QE"], final_result)
+#     final_result = calculate_distance_br(distance_type["D_QE"], result_AL)
+#
+# X, Y = np.meshgrid(list(range(image.shape[0])), list(range(image.shape[1])))
+#
+# plt.subplot(1, 3, 1)
+# plt.title("Random ones")
+# plt.imshow(image)
+# plt.subplot(1, 3, 2)
+# plt.title("Random ones")
+# plt.imshow(result_AL)
+# plt.subplot(1, 3, 3)
+# plt.imshow(final_result)
+# plt.contour(X, Y, final_result[Y, X], 8, colors='white', linewidths=.2)
+# plt.title("Random ones with D_QE")
+# plt.show()
