a1670cf15ef948c8c923a1400130c341178e378d
jonghyeok lee <30971523+hololee@users.noreply.github.com>
2020-04-03 17:52:20 +0900

    init

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ centers = tuple of centers.
 center_margin = 20
 
 # maximum_val
-N_max = 5
+N_max = np.inf
 
 
 def euclidean_distance(*xys):
@@ -36,13 +36,38 @@ def cityblock_distance(*xys):
     return distance
 
 
+def chessboard_distance(*xys):
+    """
+    :param xys: (x1, x2, y1, y2)
+    :return:
+    """
+    distance = np.max(np.abs(xys[0] - xys[1]), np.abs(xys[2] - xys[3]))
+    return distance
+
+
+def quasi_euclidean_distance(*xys):
+    """
+    :param xys: (x1, x2, y1, y2)
+    :return:
+    """
+    if np.abs(xys[0] - xys[1]) > np.abs(xys[2] - xys[3]):
+        distance = np.abs(xys[0] - xys[1]) + (np.sqrt(2) - 1) * np.abs(xys[2] - xys[3])
+    else:
+        distance = (np.sqrt(2) - 1) * np.abs(xys[0] - xys[1]) + np.abs(xys[2] - xys[3])
+    return distance
+
+
 # distance type.
-distance_type = {"D_E": euclidean_distance, "D_4": cityblock_distance, "D_8": 2, "D_QE": 3}
+distance_type = {"D_E": euclidean_distance,
+                 "D_4": cityblock_distance,
+                 "D_8": chessboard_distance,
+                 "D_QE": quasi_euclidean_distance,
+                 }
 
 
 # method ----------------------------------------------------------------------
 
-def generate_image(data=None):
+def generate_image(use_sample=True, init_img=None):
     # TODO: generate random image.
 
     sample = np.array([[0, 0, 0, 0, 0, 0, 1, 0],
@@ -54,8 +79,15 @@ def generate_image(data=None):
                        [0, 1, 0, 0, 0, 0, 0, 0],
                        [0, 1, 0, 0, 0, 0, 0, 0]
                        ], dtype=np.float)
-    if data is None:
+    if use_sample:
         data = sample
+    else:
+        data = np.zeros([init_img["size"], init_img["size"]])
+
+        x_coord = np.random.randint(0, init_img["size"], init_img["centers"])
+        y_coord = np.random.randint(0, init_img["size"], init_img["centers"])
+
+        data[y_coord, x_coord] = 1
 
     copy_image = deepcopy(data)
 
@@ -148,11 +180,43 @@ def calculate_distance_br(sel_distance, sel_image):
 
 # main script ----------------------------------------------------------------------
 
-
+# # 1. Normal sample.---------------------------------------------
 # generate random centers.
 image = generate_image()
 result_AL = calculate_distance_al(distance_type["D_4"], image)
 final_result = calculate_distance_br(distance_type["D_4"], result_AL)
 
+plt.subplot(1, 2, 1)
+plt.imshow(image)
+ax = plt.gca()
+for y in range(image.shape[0]):
+    for x in range(image.shape[1]):
+        ax.text(x, y, image[y, x], color='white', ha='center', va='center')
+plt.title("Sample image")
+
+plt.subplot(1, 2, 2)
 plt.imshow(final_result)
+ax = plt.gca()
+for y in range(final_result.shape[0]):
+    for x in range(final_result.shape[1]):
+        ax.text(x, y, final_result[y, x], color='white', ha='center', va='center')
+
+plt.title("Sample image with D_E")
+plt.show()
+
+# # 1. DE sample. ---------------------------------------------
+# generate random centers.
+image = generate_image(use_sample=False, init_img={"centers": 3, "size": 128})
+result_AL = calculate_distance_al(distance_type["D_4"], image)
+final_result = calculate_distance_br(distance_type["D_4"], result_AL)
+
+X, Y = np.meshgrid(list(range(image.shape[0])), list(range(image.shape[1])))
+
+plt.subplot(1, 2, 1)
+plt.title("Random ones")
+plt.imshow(image)
+plt.subplot(1, 2, 2)
+plt.imshow(final_result)
+# plt.contour(X, Y, final_result[Y, X], 8, colors='white', linewidth=.2)
+plt.title("Random ones with D_4")
 plt.show()
